30b6180a3cf41f1a3bc49f3bcbb20b87e9cc4e6b
kjverweel <kjverweel@student.che.nl>
2023-03-07 12:56:54 +0100

    added log getgroups

--- repositories/GetGroups.go
+++ repositories/GetGroups.go
@@ -2,17 +2,19 @@ package repositories
 
 import (
 	"github.com/jinzhu/gorm"
+	"log"
 	"main.go/models"
 )
 
-func GetGroup(groupname string) ([]models.Groups, error) {
+func GetGroup(groepname string) ([]models.Groups, error) {
 	var groups []models.Groups
-	err := db.Where("groupname = ?", groupname).Find(&groups).Error
+	err := db.Where("groepname = ?", groepname).Find(&groups).Error
 	if err != nil {
 		if gorm.IsRecordNotFoundError(err) {
 			return nil, nil // return nil if no records found
 		}
 		return nil, err
 	}
+	log.Println(groups)
 	return groups, nil
 }
