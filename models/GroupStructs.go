8e2a95e1d042adde165ed34c91909f18457becd3
kjverweel <kjverweel@student.che.nl>
2023-03-08 20:33:39 +0100

    removed typo

--- models/GroupStructs.go
+++ models/GroupStructs.go
@@ -4,6 +4,6 @@ import "gorm.io/gorm"
 
 type Groups struct {
 	gorm.Model
-	Groepname  string `gorm:"type:varchar(255);not null",`
+	Groepname  string `gorm:"type:varchar(255);not null"`
 	Groepadmin string `gorm:"type:varchar(255)"`
 }
