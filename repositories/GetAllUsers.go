d78f6934d8f38881a2400520502568ea01f5e10b
kjverweel <kjverweel@student.che.nl>
2023-03-08 13:52:09 +0100

    finished user db logic

--- repositories/GetAllUsers.go
+++ repositories/GetAllUsers.go
@@ -1,1 +1,19 @@
 package repositories
+
+import (
+	"github.com/jinzhu/gorm"
+	"log"
+)
+
+func GetAllUsers() ([]string, error) {
+	var AllUsers []string
+	err := db.Table("users").Pluck("DISTINCT user_nickname", &AllUsers).Error
+	if err != nil {
+		if gorm.IsRecordNotFoundError(err) {
+			return nil, nil // return nil if no records found
+		}
+		return nil, err
+	}
+	log.Println(AllUsers)
+	return AllUsers, nil
+}
