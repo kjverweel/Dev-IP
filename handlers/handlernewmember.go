b9aee64133543badac1ecf30780fd779c0a1b7e5
kjverweel <kjverweel@student.che.nl>
2023-03-08 22:20:10 +0100

    added cookie to handlernewmember

--- handlers/handlernewmember.go
+++ handlers/handlernewmember.go
@@ -6,9 +6,23 @@ import (
 	"main.go/models"
 	"main.go/repositories"
 	"net/http"
+	"strconv"
 )
 
 func GetNewMemberInfo(e echo.Context) error {
+	// get cookie from request
+	cookie, err := e.Cookie("User")
+	// parse cookie string value to uint
+	userId, err := strconv.ParseUint(cookie.Value, 10, 64)
+	if err != nil {
+		log.Println("handlerhome.go:Couldn't get cookie")
+		e.Render(http.StatusOK, "index", nil)
+	}
+	user := &models.Users{}
+	err = repositories.GetUser(uint(userId), &user)
+	if err != nil {
+		log.Println("handlerhome.go:Couldn't get cookie")
+	}
 
 	if e.FormValue("UserName") == "" || e.FormValue("GroupName") == "" {
 		return e.Render(http.StatusUnauthorized, "member", nil)
@@ -41,7 +55,7 @@ func GetNewMemberInfo(e echo.Context) error {
 	if groups == nil {
 		e.Render(http.StatusOK, "home", echo.Map{"Groups": "Unfortunately, there are no groups yet"})
 	}
-	err = e.Render(http.StatusOK, "home", echo.Map{"Groups": groups})
+	err = e.Render(http.StatusOK, "home", echo.Map{"Nem": user.UserNickname, "Groups": groups})
 	if err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
 	}
