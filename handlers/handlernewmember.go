99cc199ac279398d978d98341b78f34d856c1589
kjverweel <kjverweel@student.che.nl>
2023-03-08 13:49:19 +0100

    changed groups to users

--- handlers/handlernewmember.go
+++ handlers/handlernewmember.go
@@ -8,16 +8,16 @@ import (
 )
 
 func Member(e echo.Context) error {
-	groups, err := repositories.GetGroup()
+	AllUsers, err := repositories.GetAllUsers()
 	if err != nil {
 		return e.JSON(http.StatusInternalServerError, map[string]interface{}{
 			"message": "Failed to get groups",
 		})
 	}
-	err = e.Render(http.StatusOK, "member", echo.Map{"Groups": groups})
+	err = e.Render(http.StatusOK, "member", echo.Map{"Users": AllUsers})
 	if err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
 	}
-	log.Println("Succesfully made it to /groups")
+	log.Println("Succesfully made it to /member")
 	return nil
 }
