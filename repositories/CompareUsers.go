2427c6d998e33964f314959a37e026b30238ca60
kjverweel <kjverweel@student.che.nl>
2023-03-08 21:51:26 +0100

    compareusers save-file

--- repositories/CompareUsers.go
+++ repositories/CompareUsers.go
@@ -18,12 +18,15 @@ func CompareUsers(newUser *models.Users) (bool, error) {
 }
 
 func CompareUsername(GetID *models.Users) (int, error) {
+	log.Println("getID:", GetID)
 	var UserID int
-	err := db.Where("user_nickname = ?", GetID.UserNickname).First("id", &UserID).Error
+	log.Println("UserID:", UserID)
+	err := db.Table("users").Where("user_nickname = ?", GetID.UserNickname).First("id", &UserID).Error
 	if err == gorm.ErrRecordNotFound {
 		log.Println("CompareUsers.go:This is a database fault")
 	} else if err != nil {
 		log.Println("CompareUsers.go:Probably couldn't find ID")
 	}
+	log.Println(UserID)
 	return UserID, nil
 }
