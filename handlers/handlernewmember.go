6a579c878c4177b41c8fe042efebe50e3de81f67
kjverweel <kjverweel@student.che.nl>
2023-03-08 13:12:47 +0100

    added stuff to member

--- handlers/handlernewmember.go
+++ handlers/handlernewmember.go
@@ -1,1 +1,23 @@
 package handlers
+
+import (
+	"github.com/labstack/echo/v4"
+	"log"
+	"main.go/repositories"
+	"net/http"
+)
+
+func Member(e echo.Context) error {
+	groups, err := repositories.GetGroup()
+	if err != nil {
+		return e.JSON(http.StatusInternalServerError, map[string]interface{}{
+			"message": "Failed to get groups",
+		})
+	}
+	err = e.Render(http.StatusOK, "member", echo.Map{"Groups": groups})
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+	log.Println("Succesfully made it to /groups")
+	return nil
+}
