79ab1b168ae5b70e248c1b70da6ba734163669b4
kjverweel <kjverweel@student.che.nl>
2023-03-09 11:46:39 +0100

    Added base logic to CheckGroupMembers

--- repositories/CheckGroupMembers.go
+++ repositories/CheckGroupMembers.go
@@ -1,1 +1,8 @@
 package repositories
+
+import "main.go/models"
+
+func CheckGroupMembers(Groupmembers *models.Groupmembers) (bool, error) {
+	err := db.Where("user_id = ?", Groupmembers.UserID, "groep_id = ?", Groupmembers.GroepID).Error
+	return false, nil
+}
