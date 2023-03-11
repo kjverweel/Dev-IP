df69628b0277979d6983ca99e593391bc4574d35
kjverweel <kjverweel@student.che.nl>
2023-03-11 14:49:29 +0100

    added logic to function createnewpost

--- handlers/handlerscreatenewposts.go
+++ handlers/handlerscreatenewposts.go
@@ -1,1 +1,24 @@
 package handlers
+
+import (
+	"github.com/labstack/echo/v4"
+	"log"
+	"main.go/models"
+	"main.go/repositories"
+)
+
+func CreateNewPost(e echo.Context) error {
+
+	Groepname := &models.Groups{
+		Groepname: e.FormValue("GroupName"),
+	}
+
+	GroupID, err := repositories.CompareGroupname(Groepname)
+	if err != nil {
+		log.Println("handlernewmember.go:couldn't find matching ID")
+		return err
+	}
+	log.Println("handlernewmember.go:GroupID is", GroupID)
+	return nil
+
+}
