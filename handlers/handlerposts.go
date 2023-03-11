c09a75129c7988580fbbf5bf6af5f3a62ad0f8f1
kjverweel <kjverweel@student.che.nl>
2023-03-11 14:05:03 +0100

    added required logic for function posts

---
+++ handlers/handlerposts.go
@@ -0,0 +1,26 @@
+package handlers
+
+import (
+	"github.com/labstack/echo/v4"
+	"log"
+	"main.go/repositories"
+	"net/http"
+)
+
+func Posts(e echo.Context) error {
+	groups, err := repositories.GetGroup()
+	if err != nil {
+		return e.JSON(http.StatusInternalServerError, map[string]interface{}{
+			"message": "Failed to get groups",
+		})
+	}
+	if groups == nil {
+		e.Render(http.StatusOK, "home", echo.Map{"Groups": "Unfortunately, there are no groups yet"})
+	}
+	err = e.Render(http.StatusOK, "newpost", nil)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error(), echo.Map{"Groups": groups})
+	}
+	log.Println("handlerposts.go:Succesfully made it to /newpost")
+	return nil
+}
