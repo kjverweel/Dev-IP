e57f1f4d82b33710251664a1d286aaff2564b5de
kjverweel <kjverweel@student.che.nl>
2023-03-12 14:49:35 +0100

    changed .log to reduce size

--- repositories/GetPost.go
+++ repositories/GetPost.go
@@ -11,12 +11,17 @@ func GetRecentPosts() ([][]string, error) {
 	for i := 0; i < 8; i++ {
 		var post models.Posts
 		result := db.Table("Posts").Select("post_content").Order("created_at desc").Limit(1).Offset(i).Find(&post)
-		log.Println("getpost.go:", result)
+
+		if result == nil {
+			log.Println("getpost.go: Posts are empty")
+		} else {
+			log.Println("getpost.go: posts called")
+		}
+
 		if result.Error != nil {
 			return nil, result.Error
 		}
 		recentPostContents = append(recentPostContents, []string{post.PostContent})
 	}
-	log.Println(recentPostContents)
 	return recentPostContents, nil
 }
