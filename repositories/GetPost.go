f643706312d7885a468b42d1f8e00b0ead382a51
kjverweel <kjverweel@student.che.nl>
2023-03-12 12:45:30 +0100

    added new logic to getpost.go

--- repositories/GetPost.go
+++ repositories/GetPost.go
@@ -1,14 +1,21 @@
 package repositories
 
 import (
+	"log"
 	"main.go/models"
 )
 
-func GetPost() ([]models.Posts, error) {
-	var posts []models.Posts
-	result := db.Table("Posts").Select("post_content").Order("created_at desc").Limit(8).Find(&posts)
-	if result.Error != nil {
-		return nil, result.Error
+func GetRecentPosts() ([]models.Posts, error) {
+	var recentPosts []models.Posts
+
+	for i := 0; i < 8; i++ {
+		var post models.Posts
+		result := db.Table("Posts").Select("post_content").Order("created_at desc").Limit(1).Offset(i).Find(&post)
+		if result.Error != nil {
+			return nil, result.Error
+		}
+		recentPosts = append(recentPosts, post)
 	}
-	return posts, nil
+	log.Println(recentPosts)
+	return recentPosts, nil
 }
