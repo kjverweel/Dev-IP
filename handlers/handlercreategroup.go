0cb85705cedd6cc7718985c591d006a7a088f72a
kjverweel <kjverweel@student.che.nl>
2023-03-14 07:55:53 +0100

    changed groupadmin to Groupadmin_ID together with all related code

--- handlers/handlercreategroup.go
+++ handlers/handlercreategroup.go
@@ -6,12 +6,32 @@ import (
 	"main.go/models"
 	"main.go/repositories"
 	"net/http"
+	"strconv"
 )
 
 func CreateGroup(e echo.Context) error {
+	//Cookiecode
+	// get cookie from request
+	cookie, err := e.Cookie("User")
+	// parse cookie string value to uint
+	userId, err := strconv.ParseUint(cookie.Value, 10, 64)
+	if err != nil {
+		//if an error occurs in Cookiecode this usually means that the user isn't logged in properly.
+		//this e.Render causes a direct to the index page, where you can log in or register an account.
+		log.Println("handlerhome.go:Couldn't get cookie")
+		e.Render(http.StatusOK, "index", nil)
+	}
+	user := &models.Users{}
+	err = repositories.GetUser(uint(userId), &user)
+	AdminID := strconv.FormatUint(userId, 10)
+	log.Println(AdminID)
+	if err != nil {
+		log.Println("handlerhome.go:Couldn't get cookie")
+	}
+
 	newGroup := &models.Groups{
-		Groepname:  e.FormValue("Groepsnaam"),
-		Groepadmin: e.FormValue("Adminnaam"),
+		Groepname:    e.FormValue("Groepsnaam"),
+		GroepadminID: AdminID,
 	}
 	GroupExists, err := repositories.CheckGroup(newGroup)
 	if err != nil || GroupExists {
--- models/GroupStructs.go
+++ models/GroupStructs.go
@@ -4,6 +4,6 @@ import "gorm.io/gorm"
 
 type Groups struct {
 	gorm.Model
-	Groepname  string `gorm:"type:varchar(255);not null"`
-	Groepadmin string `gorm:"type:varchar(255)"`
+	Groepname    string `gorm:"type:varchar(255);not null"`
+	GroepadminID string `gorm:"type:varchar(100)"`
 }
