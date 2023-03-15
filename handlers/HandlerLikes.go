a15a167c13791dbfb0ff58114b13692f7f3ab54e
kjverweel <kjverweel@student.che.nl>
2023-03-15 22:22:08 +0100

    commit before switcheroo

--- handlers/HandlerLikes.go
+++ handlers/HandlerLikes.go
@@ -1,5 +1,22 @@
 package handlers
 
-func GetAndUpdateLikes(PostID int) {
+import (
+	"github.com/labstack/echo/v4"
+	"log"
+	"net/http"
+	"strconv"
+)
 
+func GetAndUpdateLikes(e echo.Context) error {
+	cookie, err := e.Cookie("User")
+	if err != nil {
+		log.Println("couldn't get cookie")
+		return nil
+	}
+	UserId, err := strconv.ParseUint(cookie.Value, 10, 64)
+	log.Println(UserId)
+
+	return e.Render(http.StatusOK, "home", echo.Map{"LikeCount": UserId})
 }
+
+//Needed: Post_ID, User_ID, amount of like already there, once done return like +1
