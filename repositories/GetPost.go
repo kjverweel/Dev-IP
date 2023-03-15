75a87b000b49ca45dede4f31c0e6117acc6aa3ed
kjverweel <kjverweel@student.che.nl>
2023-03-15 13:01:51 +0100

    IMAGES WORK! THEY WORK! IT WORKS

--- repositories/GetPost.go
+++ repositories/GetPost.go
@@ -9,12 +9,12 @@ func GetRecentPosts(GroepID []int) ([][]string, error) {
 
 	for i := 0; i < 8; i++ {
 		var post models.Posts
-		result := db.Table("Posts").Select("post_content").Where("groep_id IN (?)", GroepID).Order("created_at desc").Limit(1).Offset(i).Find(&post)
+		result := db.Table("Posts").Select("post_content, post_image_location").Where("groep_id IN (?)", GroepID).Order("created_at desc").Limit(1).Offset(i).Find(&post)
 
 		if result.Error != nil {
 			return nil, result.Error
 		}
-		recentPostContents = append(recentPostContents, []string{post.PostContent})
+		recentPostContents = append(recentPostContents, []string{post.PostContent, post.PostImageLocation})
 	}
 
 	return recentPostContents, nil
